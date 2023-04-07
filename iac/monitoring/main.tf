c965dbf7b264478de607aa6014feb7a36d53d648
Olang Daniel <olangdan17@gmail.com>
2023-04-07 06:27:39 +0100

    final

--- iac/monitoring/main.tf
+++ iac/monitoring/main.tf
@@ -64,3 +64,12 @@ provider "helm" {
   }
  
 }
+
+data "kubectl_path_documents" "docs" {
+    pattern = "./*.yaml"
+}
+
+resource "kubectl_manifest" "monitoring" {
+    for_each  = toset(data.kubectl_path_documents.docs.documents)
+    yaml_body = each.value
+}
